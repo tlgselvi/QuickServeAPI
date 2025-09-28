75599fbd06267bb539ea6c0fdb40470a4a2ea13e
tlgselvi <tlgselvi@gmail.com>
2025-09-29 00:21:44 +0300

    fix: remove unused connect-pg-simple import (prevent prod crash)

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,5 @@
 import express, { type Request, Response, NextFunction } from 'express';
 import session from 'express-session';
-import connectPgSimple from 'connect-pg-simple';
 import { registerRoutes } from './routes';
 import { setupVite, serveStatic, log } from './vite';
 import { pool } from './db';
